0804c79547f38f10fecf6b25e98505165823c6c4
Benjamin Reh <ich@benjaminreh.de>
2013-09-26 14:30:25 +0200

    solved the watermak issue
    Signed-off-by: Benjamin Reh <ich@benjaminreh.de>

--- capture.sh
+++ capture.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
-INPUT_PARAM="-ar 44100  -f alsa -i pulse  -f v4l2 -r 25 -i /dev/video0"
-CODEC_PARAM=" "
-OUTPUT_PARAM="-f flv -metadata streamName=myLiveStream tcp://navicular.iwr.uni-heidelberg.de:6666/live/myLiveStream"
+set -e 
+#================================
+#Settings
+DEV=/dev/video0
+# to see the supported modes run
+#  v4l2-ctl --list-formats-ext -d DEVICE
 
-ffmpeg $INPUT_PARAM $CODEC_PARAM $OUTPUT_PARAM 
+RES="640x480"
+FPS=25
+# Quality Settings, depending on your bandwith
+#  video
+V_BITRATE=600000
+
+# Watermark
+USE_WATERMARK=1
+
+
+#================================
+#putting everything together
+INPUT_PARAM="-ar 44100  -f alsa -i pulse  -f v4l2 -s $RES -r $FPS -i $DEV"
+CODEC_PARAM="-f flv -vcodec flv -b:v $V_BITRATE -qmin 1 -qmax 31"
+
+if [ "$USE_WATERMARK" = "1" ];
+then
+	WATERMARK_PARAM="-vf \"movie=watermark.png [watermark]; [in][watermark] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]\""
+	inkscape watermark.svg -e watermark.png
+else
+	WATERMARK_PARAM=""
+fi
+
+OUTPUT_PARAM="-metadata streamName=myLiveStream tcp://navicular.iwr.uni-heidelberg.de:6666/live/myLiveStream"
+FFMPEG="ffmpeg $INPUT_PARAM $WATERMARK_PARAM $CODEC_PARAM $OUTPUT_PARAM "
+
+#================================
+#runing commands
+if [ ! -e $DEV ];
+then
+	echo "Video device $DEV not available"
+	exit 
+fi
+#~ set -x
+eval $FFMPEG
